a63c8d8d07c664ce2d5fb5a8102c65f7fddcf010
NicholasPage <pagenw142@gmail.com>
2022-01-13 08:28:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,15 +10,23 @@ app = Flask(__name__)
 
 allowed_keys = ["A","B","Space","Up","Down","Left","Right"]
 
-WID = subprocess.call(["xdotool", "search", "--class", "retroarch"])
-subprocess.call(["xdotool", "windowfocus", str(WID)])
+WID = subprocess.run(["xdotool", "search", "--class", "retroarch"], capture_output=True)
+
+WID = str(WID.stdout)
+
+WID = WID.split("'")
+
+WID = WID[1]
+
+WID = WID.split('\\')
+
+WID = WID[0]
 
 @app.route('/TPP/api/v1.0/command', methods=['POST'])
 def Input_Command():
-    WID = subprocess.call(["xdotool", "search", "--class", "retroarch"])
     """"build the input for xdotool"""
-    subprocess.call(["xdotool", "windowfocus", str(WID)])
-    subprocess.call(["xdotool", "key", request.json.get('input', "")])
+    subprocess.run(["xdotool", "windowfocus", WID])
+    subprocess.run(["xdotool", "key", request.json.get('input', "")])
     return jsonify(request.json.get('input', "")), 201
 
 if __name__ == '__main__':
