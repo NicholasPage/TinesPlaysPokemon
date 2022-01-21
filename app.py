32e5867263c8ac761ae9c18ad33a79cf1d527b0d
NicholasPage <pagenw142@gmail.com>
2022-01-21 08:53:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,30 +11,25 @@ app = Flask(__name__)
 allowed_keys = ["A","B","C","Up","Down","Left","Right"]
 
 WID = subprocess.run(["xdotool", "search", "--class", "retroarch"], capture_output=True)
-
 WID = str(WID.stdout)
-
 WID = WID.split("'")
-
 WID = WID[1]
-
 WID = WID.split('\\')
-
 WID = WID[0]
-
-subprocess.run(["xdotool", "windowfocus", WID])
-time.sleep(0.1)
 
 @app.route('/TPP/api/v1/command', methods=['POST'])
 def Input_Command():
     """"build the input for xdotool"""
     if (request.json.get('input', "") in allowed_keys):
-        #subprocess.run(["xdotool", "windowfocus", WID])
-        #time.sleep(0.1)
-        subprocess.run(["xdotool", "key", request.json.get('input', "")])
-        time.sleep(0.2)
+        subprocess.run(["xdotool", "windowfocus", WID])
+        subprocess.run(["xdotool", "keydown", request.json.get('input', "")])
+        time.sleep(0.02)
+        subprocess.run(["xdotool", "keyup", request.json.get('input', "")])
+        time.sleep(0.02)
+        print(request.json.get('input', ""))
         return jsonify(request.json.get('input', "")), 200
     else:
         return jsonify('Unapproved Key Attempt'), 400
 if __name__ == '__main__':
+
      app.run(debug=True,host='0.0.0.0')
